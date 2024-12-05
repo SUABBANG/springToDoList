e1be9cf366fb65829802dfd209c6164277958391
ssuangg <115128140+ssuangg@users.noreply.github.com>
2024-12-05 11:57:34 +0900

    testupload

---
+++ src/test/java/hello_spring/springToDoList/SpringToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package hello_spring.springToDoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
